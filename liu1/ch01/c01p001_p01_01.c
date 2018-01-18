b509dfcf9b073dc8234944236e5303479fd35764
mcsdyuyu <mcsdyuyu@163.com>
2018-01-18 23:15:03 +0800

    printf and integer expressions

--- liu1/ch01/c01p001_p01_01.c
+++ liu1/ch01/c01p001_p01_01.c
@@ -3,7 +3,11 @@
 int main()
 {
     printf("%d\n", 1 + 2);
-    printf("%d\n", 3 * 6);
+    printf("%d\n", 3 - 4);
+    printf("%d\n", 5 * 6);
+    printf("%d\n", 8 / 5);  //整数除整数得到整数
+    printf("%d\n", 8 % 5);  //取余 3
+    printf("%d\n", -8 % 5);  //取余 -3
 
     return 0;
 }
