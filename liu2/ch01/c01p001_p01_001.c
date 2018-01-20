259cbb1457ac0c2cf2ea858ca9df1c0cab27ac24
mcsdyuyu <mcsdyuyu@163.com>
2018-01-20 15:23:31 +0800

    printf %d integer

---
+++ liu2/ch01/c01p001_p01_001.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n", 1 + 2);  //3
+
+    printf("%d\n", 3 - 4);  //-1
+    printf("%d\n", 5 * 6);  //30
+    printf("%d\n", 8 / 4);  //2
+    printf("%d\n", 8 / 5);  //1 ** 整数运算结果是整数 得到整数的商
+    printf("%d\n", (-8) / 5);  //-1
+
+    return 0;
+}
