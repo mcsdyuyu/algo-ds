1ad72b7f997ff85c0404084218cf22776757c9dd
mcsdyuyu <mcsdyuyu@163.com>
2018-01-24 00:01:57 +0800

    delete test01

--- gittest/test01.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("111\n");
-    printf("222\n");
-
-    return 0;
-}
--- gittest/test02.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("333\n");
-    printf("444\n");
-
-    return 0;
-}
